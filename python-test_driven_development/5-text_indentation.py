1ee2a9506547226b9e5b73eaf75c431c41c053c3
Carine-Ashimwe <carineashimwe@gmail.com>
2024-03-26 14:11:57 +0000

    4. Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,33 +1,27 @@
->>> text_indentation = __import__('5-text_indentation').text_indentation
->>> text_indentation("tomato     .   ,,,,/\?00")
-tomato     .
-<BLANKLINE>
-,,,,/\?
-<BLANKLINE>
-00
->>> text_indentation("....")
-.
-<BLANKLINE>
-.
-<BLANKLINE>
-.
-<BLANKLINE>
-.
-<BLANKLINE>
->>> text_indentation("")
+#!/usr/bin/python3
+''' Defining a text indentation function that prints indented text '''
 
->>> text_indentation(" .? ")
-.
-<BLANKLINE>
-?
-<BLANKLINE>
->>> text_indentation("     .     . ")
-.
-<BLANKLINE>
-.
-<BLANKLINE>
->>> text_indentation("  ")
 
->>> text_indentation(""")
-Traceback (most recent call last):
-SyntaxError: EOF while scanning triple-quoted string literal
+def text_indentation(text):
+    '''
+    Prints a text with 2 new lines after . ? and :
+    Ensures text is a string
+    Raises an error if text is not a string
+    '''
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in range(0, len(text)):
+        if text[i] in [".", "?", ":"]:
+            print(text[i], end="")
+            print("\n")
+        elif text[i] == " ":
+            for x in range(i, 0, -1):
+                if text[x] == " ":
+                    continue
+                elif text[x] in [".", "?", ":"]:
+                    break
+                else:
+                    print(text[i], end="")
+                    break
+        else:
+            print(text[i], end="")
